3b92f8c3eaf19defe350eacd374dd204cd727f75
Tillo <tillo.bosshart@gmail.com>
2020-09-29 07:21:29 +0200

    lm to w calculation added

--- wtolm/main.go
+++ wtolm/main.go
@@ -22,19 +22,28 @@ const Km float64 = 685
 
 func main() {
 	var θλ float64
+	var θv float64
 	var λ int
-	flag.Float64Var(&θλ, "p", 0.1, "θλ, Spectral Radiant Flux in mW")
-	flag.IntVar(&λ, "l", 550, "λ Wavelenght in nm rounded to the next")
+	flag.Float64Var(&θλ, "p", 0, "θλ, Spectral Radiant Flux in mW")
+	flag.Float64Var(&θv, "l", 0, "θv, Photopic Luminous Flux in lm")
+	flag.IntVar(&λ, "w", 550, "λ Wavelenght in nm rounded to the next")
 
 	// custom help string
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stdout, "%s\n", os.Args[0])
-		fmt.Fprintf(os.Stdout, "Smol tool to calculate lm from Watts.\n\n")
+		fmt.Fprintf(os.Stdout, "%s\n\n", os.Args[0])
+		fmt.Fprintf(os.Stdout, "Smol tool to calculate lumens from watts.\nAnd vice versa.\n\n")
 		fmt.Fprintf(os.Stdout, "Usage:\n")
 		flag.PrintDefaults()
 	}
 
 	flag.Parse()
-
-	fmt.Println(θλ * Km * lut.Vλ[λ])
+	if θλ == 0 && θv == 0 {
+		flag.Usage()
+	} else if θv == 0 {
+		fmt.Println(θλ * Km * lut.Vλ[λ])
+	} else if θλ == 0 {
+		fmt.Println(θv / (Km * lut.Vλ[λ]))
+	} else {
+		flag.Usage()
+	}
 }
