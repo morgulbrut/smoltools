29c78582b7553c5f50c3d82e5b7196bb73851989
Tillo <tillo.bosshart@gmail.com>
2021-01-12 00:09:07 +0100

    Output to file implemented

--- via2m60/converter/template.go
+++ via2m60/converter/template.go
@@ -9,20 +9,47 @@ import (
 	"github.com/morgulbrut/color256"
 )
 
+type Output struct {
+	Keymap string
+	Macros string
+}
+
+func find(slice []int, val int) bool {
+	for _, item := range slice {
+		if item == val {
+			return true
+		}
+	}
+	return false
+}
+
 func FmtLayers(lays [][]string) string {
 	var ret strings.Builder
+	// TODO:
+	neededKeys := []int{
+		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14,
+		15, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 29,
+		30, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 43,
+		45, 47, 48, 49, 50, 51, 55, 56, 58,
+		60, 61, 63, 66, 70, 71, 73, 74,
+	}
+	lineBreaks := []int{
+		15, 30, 45, 60,
+	}
+
 	ret.WriteString("keyboard.keymap = (\n")
 	for i, l := range lays {
 		ret.WriteString(fmt.Sprintf("    # layer %d\n    (\n        ", i))
 		for j, k := range l {
-			if j == 15 || j == 30 || j == 44 || j == 59 {
+			if find(lineBreaks, j) {
 				ret.WriteString("\n        ")
-
 			}
-			if k == "TRANSPARENT" {
-				ret.WriteString("___, ")
-			} else if k != "" {
-				ret.WriteString(fmt.Sprintf("%s, ", k))
+			if find(neededKeys, j) {
+				if k == "TRANSPARENT" {
+					ret.WriteString("___, ")
+				} else if k != "" {
+					ret.WriteString(fmt.Sprintf("%s, ", k))
+				}
 			}
 		}
 		ret.WriteString("\n    ),\n")
@@ -31,7 +58,7 @@ func FmtLayers(lays [][]string) string {
 	return ret.String()
 }
 
-func executeTemplate(s, temp string) {
+func ExecuteTemplate(c Output, temp string) {
 	f, err := os.Create("code_via.py")
 	if err != nil {
 		color256.PrintHiRed("ERROR: template parsing fail")
@@ -42,5 +69,5 @@ func executeTemplate(s, temp string) {
 		color256.PrintHiRed("ERROR: template parsing fail")
 		os.Exit(1)
 	}
-	t.Execute(f, s)
+	t.Execute(f, c)
 }
