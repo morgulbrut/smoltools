dbe77ad4d70b4a9bf93ff71fe9b4c5a71541bab9
Tillo <tillo.bosshart@gmail.com>
2021-01-17 23:46:43 +0100

    code clean up

--- via2m60/converter/keymap.go
+++ via2m60/converter/keymap.go
@@ -4,22 +4,14 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/morgulbrut/color256"
 	"github.com/morgulbrut/toml"
 )
-
-func PrintKeymap(km map[string]interface{}) {
-	for k, val := range km {
-		color256.PrintHiGreen("%s: %s", k, val)
-	}
-}
 
 func ReadKeymap(filename string) map[string]interface{} {
 	var result map[string]interface{}
 	// Unmarshal or Decode the JSON to the interface.
 	val := readfile(filename)
 	toml.Unmarshal(val, &result)
-
 	return result
 }
 
